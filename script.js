9fc3ec158971170e9a44ada016b8b47748d3608e
Marzi <mohammadimarziyeh6367@gmail.com>
2026-07-25 02:39:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+let studentName = "";
+
+document.getElementById("startBtn").onclick = function () {
+
+    studentName = document.getElementById("studentName").value.trim();
+
+    if (studentName === "") {
+        alert("لطفاً نام خود را وارد کن 😊");
+        return;
+    }
+
+    document.getElementById("startPage").style.display = "none";
+    document.getElementById("gamePage").style.display = "block";
+
+    document.getElementById("showName").innerHTML =
+        "🌸 " + studentName + " خوش آمدی";
+};
