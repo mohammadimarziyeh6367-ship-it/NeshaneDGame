2cc7c4ae3c3daeda94becdf49a350d7c16ec7514
Marzi <mohammadimarziyeh6367@gmail.com>
2026-07-25 18:17:07 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,104 @@
-
 let studentName = "";
+let score = 0;
 
-document.getElementById("startBtn").onclick = function () {
+const words = [
 
-    studentName = document.getElementById("studentName").value.trim();
+{word:"دَر",correct:true},
+{word:"دریا",correct:true},
+{word:"دوست",correct:true},
+{word:"دندان",correct:true},
+{word:"دست",correct:true},
+{word:"دارکوب",correct:true},
+{word:"درخت",correct:true},
+{word:"دیوار",correct:true},
+{word:"دارو",correct:true},
+{word:"دُلمه",correct:true},
+{word:"داغ",correct:true},
+{word:"دل‌درد",correct:true},
 
-    if (studentName === "") {
-        alert("لطفاً نام خود را وارد کن 😊");
-        return;
-    }
+{word:"سیب",correct:false},
+{word:"گل",correct:false},
+{word:"باد",correct:false},
+{word:"خورشید",correct:false},
+{word:"گربه",correct:false},
+{word:"ماه",correct:false}
 
-    document.getElementById("startPage").style.display = "none";
-    document.getElementById("gamePage").style.display = "block";
+];
 
-    document.getElementById("showName").innerHTML =
-        "🌸 " + studentName + " خوش آمدی";
-};
+document.getElementById("startBtn").onclick=function(){
+
+studentName=document.getElementById("studentName").value.trim();
+
+if(studentName==""){
+
+alert("نام خود را وارد کن 😊");
+
+return;
+
+}
+
+document.getElementById("startPage").style.display="none";
+
+document.getElementById("gamePage").style.display="block";
+
+document.getElementById("showName").innerHTML=
+
+"🌸 "+studentName+" خوش آمدی";
+
+createCards();
+
+}
+
+function createCards(){
+
+let box=document.getElementById("cards");
+
+box.innerHTML="";
+
+words.sort(()=>Math.random()-0.5);
+
+words.forEach(item=>{
+
+let btn=document.createElement("button");
+
+btn.className="wordCard";
+
+btn.innerHTML=item.word;
+
+btn.onclick=function(){
+
+if(btn.disabled)return;
+
+btn.disabled=true;
+
+if(item.correct){
+
+btn.classList.add("correct");
+
+score++;
+
+document.getElementById("score").innerHTML=score;
+
+document.getElementById("message").innerHTML=
+
+"🌟 آفرین! این کلمه با «د» شروع می‌شود.";
+
+}
+
+else{
+
+btn.classList.add("wrong");
+
+document.getElementById("message").innerHTML=
+
+"😊 این کلمه با «د» شروع نمی‌شود.";
+
+}
+
+}
+
+box.appendChild(btn);
+
+});
+
+}
